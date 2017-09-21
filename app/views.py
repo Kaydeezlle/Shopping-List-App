d9ddc322930633732db7bf1e5e8a10ef2ebff384
Kaydeezlle <kimanthimwanza.com>
2017-09-21 17:32:12 +0300

    Updated the paths to the css in the htmls to relative.

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('login.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 '''A module run that is used to call the app to run the webiste'''
-from app import app
+from app.views import app
 #method to call the app to run
 if __name__ == '__main__':
     app.run(debug=True)

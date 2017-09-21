5c493770f9f909851993f57cde9d1c7e7c98fbc6
Kaydeezlle <kimanthimwanza.com>
2017-09-21 11:53:41 +0300

    >Added a return user id method.

--- app/models.py
+++ app/models.py
@@ -2,13 +2,13 @@
     log in users, create a shopping list, edit the shopping list, view the shopping list and delete 
     shopping list items'''
 
-
+USER_ID = 0
 class Users(object):
-    #the user class
+    '''the user class'''
 
     users = []
     def __init__(self, user_name=None, user_email=None, user_password=None):
-        #User class variable initialization
+        '''User class variable initialization..'''
         if user_name and user_password and user_email:
             self.user_name = user_name
             self.user_email = user_email
@@ -18,9 +18,9 @@ class Users(object):
             self.user_name = None
             self.user_email = None
             self.user_password = None
-    
+
     def create_user(self, user_name, user_email, password):
-        #method for creating new users
+        '''method for creating new users'''
         for user_object in self.users:
             if user_object.user_email == user_email:
                 return "same email"
@@ -33,9 +33,16 @@ class Users(object):
             return "Okay"
 
     def login_user(self, user_email, password):
-        #method for logging in a user
+        '''method for logging in a user'''
         global USER_ID
         for member in self.users:
             if member.user_email == user_email and member.user_password == password:
                 USER_ID = self.users.index(member)
                 return True
+
+    @classmethod
+    def get_id(cls):
+        '''A return user_id method.'''
+        global USER_ID
+        return USER_ID
+            
